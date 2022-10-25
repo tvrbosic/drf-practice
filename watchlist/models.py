61c9c8c519e454060594de12b0011399813692a0
tvrbosic <tomislav.vrbosic@gmail.com>
2022-10-25 15:26:44 +0200

    Added Models Relationship

--- watchlist/models.py
+++ watchlist/models.py
@@ -3,16 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Movie(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=200)
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=30)
     about = models.CharField(max_length=200)
@@ -20,3 +10,15 @@ class StreamPlatform(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    platform = models.ForeignKey(
+        StreamPlatform, on_delete=models.CASCADE, related_name='movies')
+
+    def __str__(self):
+        return self.title
