1a84a957f872a11603bf7b7f450ca9847b0c4980
tvrbosic <tomislav.vrbosic@gmail.com>
2022-10-25 13:11:59 +0200

    Basic request handling, serializers

--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -6,20 +6,32 @@ from watchlist.models import Movie
 from watchlist.api.serializers import MovieSerializer
 
 
+def response_payload(successful, data=None, message=None, errors=None):
+    payload = {}
+    payload['status'] = 'success' if successful else 'fail'
+    if data is not None:
+        payload['data'] = data
+    if message is not None:
+        payload['message'] = message
+    if errors is not None:
+        payload['errors'] = errors
+    return payload
+
+
 @api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
-        return Response({'status': 'success', 'data': serializer.data})
+        return Response(response_payload(successful=True, data=serializer.data))
 
     if request.method == 'POST':
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'status': 'success', 'data': serializer.data})
+            return Response(response_payload(successful=True, data=serializer.data))
         else:
-            return Response({'status': 'fail', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(response_payload(successful=False, errors=serializer.errors), status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
@@ -27,36 +39,30 @@ def movie_details(request, pk):
     if request.method == 'GET':
         try:
             movie = Movie.objects.get(pk=pk)
-            serializer = MovieSerializer(movie)
-            return Response({'status': 'success', 'data': serializer.data})
         except Movie.DoesNotExist:
-            data = {}
-            data['status'] = 'fail'
-            data['error'] = 'Movie with requested id was not found!'
-            return Response(data, status=status.HTTP_404_NOT_FOUND)
+            return Response(response_payload(successful=False, message='Movie with requested id was not found!'), status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie)
+        return Response(response_payload(successful=True, data=serializer.data))
 
     if request.method == 'PUT':
         try:
             movie = Movie.objects.get(pk=pk)
-            serializer = MovieSerializer(movie, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'status': 'success', 'data': serializer.data})
-            else:
-                return Response(serializer.errors)
         except Movie.DoesNotExist:
-            data = {}
-            data['status'] = 'fail'
-            data['error'] = 'Movie with requested id was not found!'
-            return Response(data, status=status.HTTP_404_NOT_FOUND)
+            return Response(response_payload(successful=False, message='Movie with requested id was not found!'), status=status.HTTP_404_NOT_FOUND)
+
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(response_payload(successful=True, data=serializer.data))
+        else:
+            return Response(serializer.errors)
 
     if request.method == 'DELETE':
         try:
             movie = Movie.objects.get(pk=pk)
-            movie.delete()
-            return Response({'status': 'success', 'data': 'Movie successfully deleted!'})
         except Movie.DoesNotExist:
-            data = {}
-            data['status'] = 'fail'
-            data['error'] = 'Movie with requested id was not found!'
-            return Response(data, status=status.HTTP_404_NOT_FOUND)
+            return Response(response_payload(successful=False, message='Movie with requested id was not found!'), status=status.HTTP_404_NOT_FOUND)
+
+        movie.delete()
+        return Response(response_payload(successful=True, message='Movie successfully deleted!'))
