d1c37f2cef744a40871e5b5ab0205bca390803d5
tvrbosic <tomislav.vrbosic@gmail.com>
2022-10-25 13:50:17 +0200

    Three types of validations on serializer

--- watchlist/serializers.py
+++ watchlist/serializers.py
@@ -2,10 +2,17 @@ from rest_framework import serializers
 
 from .models import Movie
 
+# Defined custom field level validator
+
+
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short!")
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -19,3 +26,18 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # Field-level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     else:
+    #         return value
+
+    # Object-level validation
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(
+                'Name and Description should not be the same!')
+        else:
+            return data
